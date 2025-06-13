ca21eb955d1b68f97aef22d107bcdf733449e696
ToDoList <christian123star@gmail.com>
2025-06-13 11:55:38 +0700

    Update NGROK

--- src/controllers/cv.controller.ts
+++ src/controllers/cv.controller.ts
@@ -43,7 +43,7 @@ class CvController {
 
       // --- PANGGIL API MODEL AI DI SINI ---
       const aiModelApiUrl =
-        process.env.AI_MODEL_API_URL || "https://01fc-2405-8180-a03-fbce-2dfc-6ab3-9366-4688.ngrok-free.app/analyze_cv";
+        process.env.AI_MODEL_API_URL || "https://a07b-2405-8180-a03-fbce-846e-3bdc-f6cd-129d.ngrok-free.app/analyze_cv";
 
       const dataForAiModel = {
         id: newCv.id, // ID CV yang baru dibuat
